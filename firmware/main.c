02eed4cd57478fc0e6ed9f094270a3f7ef91f498
Henning Bekel <h.bekel@googlemail.com>
2016-01-26 22:08:05 +0100

    Firmware: Added some comments, added macro to convert usecs to cpu cycles

--- firmware/main.c
+++ firmware/main.c
@@ -1,21 +1,25 @@
 #define F_CPU 20000000UL
+#define TICKS_PER_USEC F_CPU/1000000UL
+
 #define Wait() while(!(SPSR & (1<<SPIF)))
+#define US(a) a*TICKS_PER_USEC
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-//LM1881 CSYNC -> INT0
-//LM1881 VSYNC -> INT1
+// LM1881 CSYNC -> INT0 (Pin 4)
+// LM1881 VSYNC -> INT1 (Pin 5)
+// Atmega OUT   -> MOSI (Pin 17)
 
 int line=0;
-uint8_t test = 0b11100000;
+uint8_t test = 0b10110001;
 
 ISR(INT1_vect) {
   TCNT1 = 0; //start the timer
 
-  //wait 180us for the vertical blanking period to end 3640/3676 seems to be good
-  while(TCNT1<3640);  
+  //wait 180us for the vertical blanking period to end
+  while(TCNT1<US(180));  
   line=0;
 }
 
@@ -24,8 +28,28 @@ ISR(INT0_vect) {
   
   if (line > 36 && line < 238) {	
     TCNT1=0;
-    while (TCNT1 < 1100);
-    SPDR = test; Wait();    
+    
+    // horizontal gap before first visible pixel?
+    // theoretically this should be sync 4us + back porch 8us = 12us +- x
+    // given that the lm1881 triggers CSYNC on the falling edge of the sync
+    // x -= some time until we get here (interrupt overhead + code above)
+    // x += some time until first visible pixel... include a safety margin
+    
+    while (TCNT1 < US(12)); 
+
+    // are my pixels square? How long is a single SPI bit pulse?
+    // is there a gap between bytes? (overhead of SPI/wait)
+    // how many bytes fit on a line?
+    // what does low/high result in?
+    // do i have enough time for pointing into a character buffer?
+    // (including pointer arithmetic?)
+
+    // try four bytes for a start...
+    
+    SPDR = test; Wait();  
+    SPDR = test; Wait();  
+    SPDR = test; Wait();  
+    SPDR = test; Wait();            
   }
 }
 
