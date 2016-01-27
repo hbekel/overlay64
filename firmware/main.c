8d7153d9f869d87f7ebce547ecb4132a02210041
Henning Bekel <h.bekel@googlemail.com>
2016-01-27 19:15:04 +0100

    Firmware: First working implementation, displays "C64" in upper left

--- firmware/main.c
+++ firmware/main.c
@@ -1,55 +1,51 @@
-#define F_CPU 20000000UL
+#define F_CPU 16000000UL
 #define TICKS_PER_USEC F_CPU/1000000UL
 
 #define Wait() while(!(SPSR & (1<<SPIF)))
-#define US(a) a*TICKS_PER_USEC
+#define US(a) a*(TICKS_PER_USEC)
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 
 // LM1881 CSYNC -> INT0 (Pin 4)
 // LM1881 VSYNC -> INT1 (Pin 5)
 // Atmega OUT   -> MOSI (Pin 17)
 
-int line=0;
-uint8_t test = 0b10110001;
+static volatile uint16_t line=0;
+uint8_t test[3*7] = {
+  0b11111110, 0b11111110, 0b11000110,
+  0b11000000, 0b11000000, 0b11000110,
+  0b11000000, 0b11000000, 0b11000110,
+  0b11000000, 0b11111110, 0b11111110,
+  0b11000000, 0b11000110, 0b00000110,   
+  0b11000000, 0b11000110, 0b00000110,
+  0b11111110, 0b11111110, 0b00000110,  
+};
 
 ISR(INT1_vect) {
-  TCNT1 = 0; //start the timer
-
   //wait 180us for the vertical blanking period to end
-  while(TCNT1<US(180));  
+  TCNT1 = 0;
+  while(TCNT1<US(180));
   line=0;
 }
 
 ISR(INT0_vect) {
+  uint16_t tmp;
   line++;
   
-  if (line > 36 && line < 238) {	
-    TCNT1=0;
-    
-    // horizontal gap before first visible pixel?
-    // theoretically this should be sync 4us + back porch 8us = 12us +- x
-    // given that the lm1881 triggers CSYNC on the falling edge of the sync
-    // x -= some time until we get here (interrupt overhead + code above)
-    // x += some time until first visible pixel... include a safety margin
-    
-    while (TCNT1 < US(12)); 
+  TCNT1=0;  
 
-    // are my pixels square? How long is a single SPI bit pulse?
-    // is there a gap between bytes? (overhead of SPI/wait)
-    // how many bytes fit on a line?
-    // what does low/high result in?
-    // do i have enough time for pointing into a character buffer?
-    // (including pointer arithmetic?)
+  if(line >= 34 && line <= 40) {
 
-    // try four bytes for a start...
-    
-    SPDR = test; Wait();  
-    SPDR = test; Wait();  
-    SPDR = test; Wait();  
-    SPDR = test; Wait();            
+    while(TCNT1<US(9));   
+
+    tmp = (line-34)*3;
+    SPDR = test[tmp+0]; _delay_us(2);
+    SPDR = test[tmp+1]; _delay_us(2);
+    SPDR = test[tmp+2]; _delay_us(2);
+    return;
   }
 }
 
@@ -58,16 +54,15 @@ int setup() {
   TIMSK1=0;
   OCR1A = 0xffff;
   
-  EICRA = (0<<ISC01) + (0<<ISC11); // Set interrupt on falling edge
-  EIMSK = (1<<INT0) + (1<<INT1);   // Enable interrupts for int0 and int1
+  EICRA = (1<<ISC01) | (1<<ISC11); // Set interrupt on falling edge
+  EIMSK = (1<<INT0) | (1<<INT1);   // Enable interrupts for int0 and int1
 
   ADCSRA&=~(1<<ADEN); // Turn off ADC
 
   //Setup SPI
   SPDR = 0;
-  DDRB = (1<<DDB3) | (1<<DDB5);
-  SPCR = (1<<SPE) | (1<<MSTR) | (1<<CPHA) | (0<<CPOL);
-  SPSR = (1<<SPI2X);
+  DDRB = (1<<DDB2) | (1<<DDB3);
+  SPCR = (1<<SPE) | (1<<MSTR) | (1<<CPHA) | (1<<CPOL);
 
   sei();
   set_sleep_mode(SLEEP_MODE_IDLE);
