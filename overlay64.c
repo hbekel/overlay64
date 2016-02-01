8cb97190198f4b333fab9bb32a3ee49718f4b369
Henning Bekel <h.bekel@googlemail.com>
2016-02-01 21:07:45 +0100

    Software: Fixed main routine, now input format determines output format

--- overlay64.c
+++ overlay64.c
@@ -6,14 +6,18 @@ int main(int argc, char **argv) {
 
   config = Config_new();
 
-  if((Config_read(config, stdin) || Config_parse(config, stdin))) {
-
-    Config_write(config, stdout);
-
-    fprintf(stderr, "memory footprint is %d bytes\n", Config_get_footprint(config));
-    
-    return EXIT_SUCCESS;
+  if(Config_read(config, stdin)) {
+    Config_print(config, stdout);
   }
-  return EXIT_FAILURE;
+  else if(Config_parse(config, stdin)) {
+    Config_write(config, stdout);
+  }
+  else {  
+    return EXIT_FAILURE;
+  }
+
+  fprintf(stderr, "memory footprint is %d bytes\n", Config_get_footprint(config));
+    
+  return EXIT_SUCCESS;
 }
 
