6594f6d167724d3ba85fd0444d0320b03b887fa5
Henning Bekel <h.bekel@googlemail.com>
2016-02-02 20:51:12 +0100

    Firmware: Code cleanups, added more comments

--- firmware/main.c
+++ firmware/main.c
@@ -5,7 +5,6 @@
 #define F_CPU 20000000UL
 #define TICKS_PER_USEC F_CPU/1000000UL
 #define US(n) n*(TICKS_PER_USEC)
-
 #define NOP() do { __asm__ __volatile__ ("nop"); } while (0)
 #define NOPS(n) for(uint8_t i=0; i<n; i++) NOP()
 
@@ -13,7 +12,6 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-#include "main.h"
 #include "font.h"
 #include "eeprom.h"
 #include "config.h"
@@ -22,11 +20,62 @@
 #define MOSI (1<<PB3)
 #define SS   (1<<PB2)
 
-volatile uint8_t frame;     // frame counter used for timing the auto-disable feature
+volatile uint8_t frame;     // frame counter for timing the auto-disable feature
 volatile uint16_t scanline; // current scanline of the whole video frame
 volatile uint8_t enabled;   // whether or not the display is currently enabled
 
 volatile Config* config;    // Configuration read from eeprom
+
+//------------------------------------------------------------------------------
+
+static void init() {
+
+  // Create config and assign ports
+  config = Config_new_with_ports(&PINB, &PINC, &PIND);
+
+  // Read config from eeprom
+  Config_read(config, &eeprom);
+  
+  // Setup Timer1
+  TCCR1B = (1<<CS10);              // Run at system clock 
+  TIMSK1 = 0;                      // Disable all timer interrupts
+
+  // Setup Interrupts
+  EICRA = (1<<ISC01) | (1<<ISC11); // Set interrupt on falling edge
+  EIMSK = (1<<INT0) | (1<<INT1);   // Enable interrupts for int0 and int1
+
+  // Turn off ADC  
+  ADCSRA&=~(1<<ADEN); 
+
+  // Setup SPI 
+  DDRB = SS | MOSI;                // /SS and MOSI as outputs
+  SPDR = 0;
+  SPCR =
+    (1<<SPE) | (1<<MSTR) |         // Enable SPI as Master
+    (1<<CPHA) | (1<<CPOL);         // Setup with falling edge of SCK
+  SPSR = (1<<SPI2X);               // Double speed
+  
+  // Setup /OE (PD7) as input
+  DDRD  &= ~OE;
+  PORTD |= OE;
+
+  // Setup Inputs
+  DDRB  &= ~((1<<PB0) | (1<<PB1));
+  PORTB |=  ((1<<PB0) | (1<<PB1));
+
+  DDRC  &= ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3) | (1<<PC4) | (1<<PC5));
+  PORTC |=  ((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3) | (1<<PC4) | (1<<PC5));
+
+  DDRD  &= ~((1<<PD0) | (1<<PD1) | (1<<PD4) | (1<<PD5) | (1<<PD6));
+  PORTD |=  ((1<<PD0) | (1<<PD1) | (1<<PD4) | (1<<PD5) | (1<<PD6));
+  
+  //Enable Interrputs
+  sei();
+    
+  // Setup sleep mode
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+}
 
 //------------------------------------------------------------------------------
 
@@ -39,7 +88,7 @@ ISR(INT1_vect) { // VSYNC (each frame)...
   frame++;
   
   // Get input and update screen according to config
-  // This must happen within 180us = 3600 cycles @ 16MHz
+  // This must happen within 180us = 3600 cycles @ 20MHz
 
   Config_apply(config);
 
@@ -101,58 +150,9 @@ ISR(INT0_vect) { // HSYNC (each line)...
 
 //------------------------------------------------------------------------------
 
-static void SetupHardware() {
-
-  // Setup Timer1
-  TCCR1B = (1<<CS10);              // Run at system clock 
-  TIMSK1 = 0;                      // Disable all timer interrupts
-
-  // Setup Interrupts
-  EICRA = (1<<ISC01) | (1<<ISC11); // Set interrupt on falling edge
-  EIMSK = (1<<INT0) | (1<<INT1);   // Enable interrupts for int0 and int1
-
-  // Turn off ADC  
-  ADCSRA&=~(1<<ADEN); 
-
-  // Setup SPI 
-  DDRB = SS | MOSI;                // /SS and MOSI as outputs
-  SPDR = 0;
-  SPCR =
-    (1<<SPE) | (1<<MSTR) |         // Enable SPI as Master
-    (1<<CPHA) | (1<<CPOL);         // Setup with falling edge of SCK
-  SPSR = (1<<SPI2X);               // Double speed
-  
-  // Setup /OE (PD7)
-  DDRD  &= ~OE;
-  PORTD |= OE;
-
-  // Setup Inputs
-  DDRB  &= ~((1<<PB0) | (1<<PB1));
-  PORTB |=  ((1<<PB0) | (1<<PB1));
-
-  DDRC  &= ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3) | (1<<PC4) | (1<<PC5));
-  PORTC |=  ((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3) | (1<<PC4) | (1<<PC5));
-
-  DDRD  &= ~((1<<PD0) | (1<<PD1) | (1<<PD4) | (1<<PD5) | (1<<PD6));
-  PORTD |=  ((1<<PD0) | (1<<PD1) | (1<<PD4) | (1<<PD5) | (1<<PD6));
-  
-  //Enable Interrputs
-  sei();
-    
-  // Setup sleep mode
-  set_sleep_mode(SLEEP_MODE_IDLE);
-  sleep_enable();
-}
-
-//------------------------------------------------------------------------------
-
 int main(void) {
 
-  config = Config_new_with_ports(&PINB, &PINC, &PIND);
-  Config_read(config, &eeprom);
-
-  SetupHardware();
-  
+  init();
   while(1) sleep_mode();
   return 0;    
 }
--- firmware/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-static void SetupHardware(void);
-
-#endif // MAIN_H
