08fc7846e4595a07f173d91fa2582b99f4632959
Henning Bekel <h.bekel@googlemail.com>
2016-02-08 20:07:41 +0100

    Software: Improved warnings about used/free eeprom/sram memory

--- overlay64.c
+++ overlay64.c
@@ -13,14 +13,17 @@ int main(int argc, char **argv) {
   }
   else if(Config_parse(config, stdin)) {
     Config_write(config, stdout);
-    fprintf(stderr, "EEPROM:\t%4d bytes\n", written);
+    fprintf(stderr, "EEPROM:\t%4d of  512 bytes used (%4d bytes free)\n",
+            written, 512-written);
   }
   else {  
     return EXIT_FAILURE;
   }
 
-  fprintf(stderr, "SRAM:\t%4d bytes\n",
-          Config_get_footprint(config));
+  uint16_t footprint = Config_get_footprint(config);
+  
+  fprintf(stderr, "SRAM:\t%4d of 2048 bytes used (%4d bytes free)\n",
+          footprint, 2048-footprint);
   
   Config_free(config);  
   return EXIT_SUCCESS;
